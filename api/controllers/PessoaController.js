6d0630d5dad301c3d4f9990c8e2ef383ddef2f56
fdolzanes1 <fdolzanes1@gmail.com>
2022-08-18 13:11:39 -0400

    adicionando os metodos atualizar e deletar pessoa

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -23,6 +23,57 @@ class PessoaController {
       return res.status(500).json( {"error": err} );
     }
   }
+
+  static async create(req, res) {
+    const novaPessoa = req.body;
+    try {
+      const pessoaCriada = await db.Pessoas.create(novaPessoa);
+      return res.status(201).json( {pessoaCriada} );
+    } catch (err) {
+      return res.status(500).json( {"error": err} );
+    }
+  }
+
+  static async update(req, res) {
+    const { id } = req.params;
+    const pessoa = req.body;
+    try {
+      await db.Pessoas.update(
+        pessoa,{ 
+          where : { 
+            id: Number(id)
+          }
+        }
+      )
+      const pessoaAtualizado = await db.Pessoas.findOne({ 
+        where: {
+          id: Number(id)
+        } 
+      });
+      return res.status(200).json({
+        mensagem:"Pessoa Atualizada com Sucesso",
+        pessoaAtualizado
+      });
+    } catch (err) {
+      return res.status(500).json( {"error": err} );
+    }
+  }
+
+  static async delete(req, res) {
+    const { id } = req.params;
+    try {
+      await db.Pessoas.destroy({ 
+        where : { 
+          id: Number(id)
+        }
+      })
+      return res.status(200).json({
+        mensagem:"Pessoa Excluida com Sucesso",
+      });
+    } catch (err) {
+      return res.status(500).json( {"error": err} );
+    }
+  }
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoutes.js
+++ api/routes/pessoasRoutes.js
@@ -4,5 +4,8 @@ const pessoaController = require('../controllers/PessoaController');
 
 router.get('/pessoas', pessoaController.listAll);
 router.get('/pessoas/:id', pessoaController.listById);
+router.post('/pessoas', pessoaController.create);
+router.put('/pessoas/:id', pessoaController.update);
+router.delete('/pessoas/:id', pessoaController.delete);
 
 module.exports = router;
