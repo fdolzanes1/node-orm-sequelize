e205062e803d56bd870e7c3f85031205203ab462
fdolzanes1 <fdolzanes1@gmail.com>
2022-08-24 12:09:12 -0400

    add metodos de update e delete

--- api/controllers/NiveisController.js
+++ api/controllers/NiveisController.js
@@ -33,6 +33,48 @@ class NiveisController {
       return res.status(500).json( {"error": err} );
     }
   }
+
+  static async update(req, res) {
+    const { id } = req.params;
+    const nivel = req.body;
+    try {
+      await db.Niveis.update(
+        nivel,{ 
+          where : { 
+            id: Number(id)
+          }
+        }
+      )
+      const nivelAtualizado = await db.Niveis.findOne({ 
+        where: {
+          id: Number(id)
+        } 
+      });
+      return res.status(200).json({
+        mensagem:"Nivel Atualizada com Sucesso",
+        nivelAtualizado
+      });
+    } catch (err) {
+      return res.status(500).json( {"error": err} );
+    }
+  }
+
+  static async delete(req, res) {
+    const { id } = req.params;
+    try {
+      await db.Niveis.destroy({ 
+        where : { 
+          id: Number(id)
+        }
+      })
+      return res.status(200).json({
+        mensagem:"Nivel Excluida com Sucesso",
+      });
+    } catch (err) {
+      return res.status(500).json( {"error": err} );
+    }
+  }
+
 }
 
 module.exports = NiveisController;
--- api/routes/niveisRoutes.js
+++ api/routes/niveisRoutes.js
@@ -5,5 +5,7 @@ const niveisController = require('../controllers/NiveisController');
 router.get('/niveis', niveisController.listAll);
 router.get('/niveis/:id', niveisController.listById);
 router.post('/niveis', niveisController.create);
+router.put('/niveis/:id', niveisController.update);
+router.delete('/niveis/:id', niveisController.delete);
 
 module.exports = router;
